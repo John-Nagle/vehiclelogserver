c122a2638ba322197bd83456bcf05713d5acc9b8
John Nagle <nagle@sitetruth.com>
2018-03-16 11:14:33 -0700

    Add summary column

--- vehicledb.sql
+++ vehicledb.sql
@@ -25,7 +25,10 @@ CREATE TABLE IF NOT EXISTS events (
 	eventtype       VARCHAR(20) NOT NULL,       -- STARTUP, SHUTDOWN, etc.
 	msg             TEXT,                       -- human-readable message
 	auxval          FLOAT NOT NULL,             -- some other value associated with the event type
+	                                            -- summarization status, set by summarizer
+	summary         ENUM('unchecked', 'complete', 'incomplete', 'junk') NOT NULL DEFAULT 'unchecked',
 	INDEX(tripid),
 	UNIQUE INDEX(tripid, serial),               -- catch dups at insert time
-	INDEX(eventtype)
+	INDEX(eventtype),
+	INDEX(summary)
 ) ENGINE InnoDB;
